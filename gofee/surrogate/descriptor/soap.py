be6c8c5b29b2d88409682f3ccd5fe06c5911ac5c
spayongmasan <51086623+mind-scraper@users.noreply.github.com>
2023-11-18 18:10:21 +0900

    Update soap.py

--- gofee/surrogate/descriptor/soap.py
+++ gofee/surrogate/descriptor/soap.py
@@ -11,10 +11,10 @@ class soap():
 
     def get_feature(self, a):
         feature = self.descriptor.create(a)
-        feature = feature.flatten()
+        feature = feature.reshape(1, -1)
         return feature
     
     def get_featureGradient(self, a):
         derivatives, descriptors = self.descriptor.derivatives(a)
-        derivatives = derivatives.flatten()
+        derivatives = derivatives.reshape(1, len(a), 3, -1)
         return derivatives
